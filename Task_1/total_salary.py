7741c78626589a9b0a3ad092aca65e7ec2cf12b4
NutSheLL <holnaz1337@gmail.com>
2024-07-14 14:35:57 +0300

    Version 1.0

--- Task_1/total_salary.py
+++ Task_1/total_salary.py
@@ -10,15 +10,16 @@ print(f"Загальна сума заробітної плати: {total}, Се
 def total_salary(path):
     # Функція total_salary(path) має приймати один аргумент - шлях до текстового файлу (path).
     # Функція повинна аналізувати файл, обчислювати загальну та середню суму заробітної плати.
-    path = Path("Salaries.txt")
+    path = Path("Task_1\Salaries.txt")
     try:
           with open(file=path, mode='r', encoding='utf-8', ) as salaries:
-            #total
             #average
-            lines = [el.strip(',') for el in salaries.readlines()]
+            lines = [el.strip(",") for el in salaries.readlines()]
+            coursor_salary = salaries.tell()
+            print(coursor_salary)
             print(lines)
     except FileNotFoundError as f:
         print(f"We didn't find such file - {f}")
 
-total_salary("Salaries.txt")
+total_salary("Task_1\Salaries.txt")
 
