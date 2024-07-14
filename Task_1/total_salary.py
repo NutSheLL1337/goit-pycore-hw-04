e655803c9a8234cdd96567dd6a7f160dc74971d9
NutSheLL <holnaz1337@gmail.com>
2024-07-14 16:25:45 +0300

    Final Version 2.0

--- Task_1/total_salary.py
+++ Task_1/total_salary.py
@@ -1,25 +1,23 @@
 from pathlib import Path
 
-'''
-    total, average = total_salary("path/to/salary_file.txt")
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-
-'''
-
-
 def total_salary(path):
-    # Функція total_salary(path) має приймати один аргумент - шлях до текстового файлу (path).
-    # Функція повинна аналізувати файл, обчислювати загальну та середню суму заробітної плати.
-    path = Path("Task_1\Salaries.txt")
     try:
-          with open(file=path, mode='r', encoding='utf-8', ) as salaries:
-            #average
-            lines = [el.strip(",") for el in salaries.readlines()]
-            coursor_salary = salaries.tell()
-            print(coursor_salary)
-            print(lines)
+        path = Path(path)
+        with open(file=path, mode='r', encoding='utf-8') as salaries:
+            lines = [el.strip() for el in salaries]
+            salary_list = [int(line.split(',')[1]) for line in lines]
+            
+            total = sum(salary_list)
+            average = total / len(salary_list)
+            
+            return total, average
     except FileNotFoundError as f:
         print(f"We didn't find such file - {f}")
+        return None, None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None, None
 
-total_salary("Task_1\Salaries.txt")
 
+total, average = total_salary("Task_1\Salaries.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
