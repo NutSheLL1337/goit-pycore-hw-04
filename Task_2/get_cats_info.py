d28d0eb1531f96df3ec8b4c496c10a3df192b4ef
NutSheLL <holnaz1337@gmail.com>
2024-07-14 17:15:07 +0300

    Version 1.0

---
+++ Task_2/get_cats_info.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+def get_cats_info(path):
+    path = Path(path)
+    try:
+        with open(file=path, mode='r', encoding='utf-8') as cats_info:
+            lines_strip = [elem.strip() for elem in cats_info]
+            string_split = [line.split(',') for line in lines_strip]
+            result_list = [{"id": item[0], "name": item[1], "age": item[2]} for item in string_split]                    
+            return result_list
+    except Exception as e:
+        print(f"Unexpected error appeared: {e}")
+
+cats_info = get_cats_info("Task_2\cats_info.txt")
+print(cats_info)
